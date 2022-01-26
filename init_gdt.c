827b9cabc80bb678d63344b43e02e1f295134906
Harold Yang <>
2022-01-26 22:50:30 +0800

    ~

--- init_gdt.c
+++ init_gdt.c
@@ -1,8 +1,7 @@
 #include <stdint.h>
 
-#define GDT_LENGTH 5
+#define GDT_LENGTH 5 //Entry number
 
-// 全局描述符类型
 typedef
 struct gdt_entry_t {
     uint16_t limit_low; // 段界限 15 ～ 0
@@ -20,39 +19,34 @@ struct gdt_ptr_t {
     uint32_t base; // 全局描述符表 32 位基地址
 } __attribute__((packed)) gdt_ptr_t;
 
-// 初始化全局描述符表
 void init_gdt();
 
-// GDT 加载到 GDTR 的函数汇编实现[]
 extern void gdt_flush(uint32_t);
 
-// 全局描述符表定义
 gdt_entry_t gdt_entries[GDT_LENGTH];
 
 
 static void gdt_set_gate(int32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran);
 
 
-// 初始化全局描述符表
+
 void init_gdt()
 {
 
     gdt_ptr_t gdt_ptr={.limit=sizeof(gdt_entry_t) * GDT_LENGTH - 1,
                        .base = (uint32_t)&gdt_entries};
 
-    // 采用 Intel 平坦模型
-    gdt_set_gate(0, 0, 0, 0, 0); // 按照 Intel 文档要求，第一个描述符必须全 0
-    gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); // 指令段
-    gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF); // 数据段
-    gdt_set_gate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); // 用户模式代码段
-    gdt_set_gate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); // 用户模式数据段
+    gdt_set_gate(0, 0, 0, 0, 0); // NULL
+    gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); // os,code
+    gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF); // os,data
+    gdt_set_gate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); // user,code
+    gdt_set_gate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); // user,data
 
-    // 加载全局描述符表地址到 GPTR 寄存器
+
     gdt_flush((uint32_t)&gdt_ptr);
 }
 
-// 全局描述符表构造函数，根据下标构造
-// 参数分别是数组下标、基地址、限长、访问标志，其它访问标志
+
 static void gdt_set_gate(int32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran)
 {
     gdt_entries[num].base_low = (base & 0xFFFF);
